4933cd60d02213e7f767b4d98ec1c67d04dd6643
Nguyễn Viết Hiếu <36782331+hieunvph04479@users.noreply.github.com>
2020-05-29 15:35:10 +0700

    đổi cấu trúc table.index và cấu hình lại form theo dạng cột

--- src/Http/Controllers/PageController.php
+++ src/Http/Controllers/PageController.php
@@ -24,13 +24,14 @@ class PageController extends AdminController
      * @return \Illuminate\Http\Response
      */
     public function index(Request $requests) {
-    	$listdata = new ListData($requests, $this->models, 'Page::table', $this->has_locale);
+    	$listdata = new ListData($requests, $this->models, 'Page::table.index', $this->has_locale);
         // Build Form tìm kiếm
         $listdata->search('name', 'Tên', 'string');
+        $listdata->search('created_at', 'Ngày tạo', 'range');
         $listdata->search('status', 'Trạng thái', 'array', config('app.status'));
         // Build các button hành động
-        $listdata->btnAction('status', 1, __('Table::table.active'), 'primary', 'fas fa-edit');
-        $listdata->btnAction('status', 0, __('Table::table.no_active'), 'warning', 'fas fa-edit');
+        $listdata->btnAction('status', 1, __('Table::table.active'), 'success', 'fas fa-edit');
+        $listdata->btnAction('status', 0, __('Table::table.no_active'), 'info', 'fas fa-window-close');
         $listdata->btnAction('delete', -1, __('Table::table.trash'), 'danger', 'fas fa-trash');
         // Build bảng
         $listdata->add('name', 'Tên', 1);
@@ -51,13 +52,22 @@ class PageController extends AdminController
     public function create() {
         // Khởi tạo form
         $form = new Form;
-        $form->text('name', '', 1, 'Tiêu đề');
-        $form->slug('slug', '', 1, 'Đường dẫn');
-        $form->editor('detail', '', 0, 'Nội dung');
-        $form->checkbox('status', 1, 1, 'Trạng thái');
-        $form->action('add');
+
+        $form->card('col-lg-9');
+            $form->lang($this->table_name);
+            $form->text('name', '', 1, 'Tiêu đề');
+            $form->slug('slug', '', 1, 'Đường dẫn');
+            $form->editor('detail', '', 0, 'Nội dung');
+        $form->endCard();
+
+        $form->card('col-lg-3', '');
+            $form->action('add');
+            $form->radio('status', 1, 'Trạng thái', config('app.status'));
+        $form->endCard();
+
+        $form->hasFullForm();
         // Hiển thị form tại view
-        return $form->render('create');
+        return $form->render('create_multi_col');
     }
 
     /**
@@ -109,15 +119,24 @@ class PageController extends AdminController
         $data_edit = $this->models->where('id', $id)->first();
         // Khởi tạo form
         $form = new Form;
-        $form->text('name', $data_edit->name, 1, 'Tiêu đề');
-        $form->slug('slug', $data_edit->slug, 1, 'Đường dẫn', '', 'false');
-        $form->editor('detail', $data_edit->detail, 0, 'Nội dung');
-        $form->checkbox('status', $data_edit->status, 1, 'Trạng thái');
-        // lấy link xem
-        $link = (config('app.page_models')) ? config('app.page_models')::where('id', $id)->first()->getUrl() : '';
-        $form->action('edit', $link);
+
+        $form->card('col-lg-9');
+            $form->text('name', $data_edit->name, 1, 'Tiêu đề');
+            $form->slug('slug', $data_edit->slug, 1, 'Đường dẫn', '', 'false');
+            $form->editor('detail', $data_edit->detail, 0, 'Nội dung');
+        $form->endCard();
+
+        $form->card('col-lg-3', '');
+            // lấy link xem
+            $link = (config('app.page_models')) ? config('app.page_models')::where('id', $id)->first()->getUrl() : '';
+            $form->action('edit', $link);
+            $form->radio('status', $data_edit->status, 'Trạng thái', config('app.status'));
+        $form->endCard();
+
+        $form->hasFullForm();
+
         // Hiển thị form tại view
-        return $form->render('edit', compact('id'));
+        return $form->render('edit_multi_col', compact('id'));
     }
 
     /**
