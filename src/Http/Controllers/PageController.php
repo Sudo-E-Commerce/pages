9d1ed174bbe0f9eaec2bf58181bcdc2a8a79d7ef
Nguyễn Viết Hiếu <36782331+hieunvph04479@users.noreply.github.com>
2020-10-07 13:57:36 +0700

    [Translate] Đa ngôn ngữ tại package Translate

--- src/Http/Controllers/PageController.php
+++ src/Http/Controllers/PageController.php
@@ -30,9 +30,9 @@ class PageController extends AdminController
         $listdata->search('created_at', 'Ngày tạo', 'range');
         $listdata->search('status', 'Trạng thái', 'array', config('app.status'));
         // Build các button hành động
-        $listdata->btnAction('status', 1, __('Table::table.active'), 'success', 'fas fa-edit');
-        $listdata->btnAction('status', 0, __('Table::table.no_active'), 'info', 'fas fa-window-close');
-        $listdata->btnAction('delete', -1, __('Table::table.trash'), 'danger', 'fas fa-trash');
+        $listdata->btnAction('status', 1, __('Translate::table.active'), 'success', 'fas fa-edit');
+        $listdata->btnAction('status', 0, __('Translate::table.no_active'), 'info', 'fas fa-window-close');
+        $listdata->btnAction('delete', -1, __('Translate::table.trash'), 'danger', 'fas fa-trash');
         // Build bảng
         $listdata->add('name', 'Tên', 1);
         $listdata->add('', 'Thời gian', 0, 'time');
@@ -94,7 +94,7 @@ class PageController extends AdminController
         // Điều hướng
         return redirect(route('admin.'.$this->table_name.'.'.$redirect, $id))->with([
             'type' => 'success',
-            'message' => __('Core::admin.create_success')
+            'message' => __('Translate::admin.create_success')
         ]);
     }
 
@@ -166,7 +166,7 @@ class PageController extends AdminController
         // Điều hướng
         return redirect(route('admin.'.$this->table_name.'.'.$redirect, $id))->with([
             'type' => 'success',
-            'message' => __('Core::admin.update_success')
+            'message' => __('Translate::admin.update_success')
         ]);
     }
 
